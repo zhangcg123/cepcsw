0729cbc02146662ccebd3289bf30f7a9aa2304a6
lintao@ihep.ac.cn <lintao@ihep.ac.cn>
2024-06-20 17:37:23 +0000

    Optimize cmake and reduce the find_package.

--- cmake/CEPCSWDependencies.cmake
+++ cmake/CEPCSWDependencies.cmake
@@ -20,22 +20,31 @@ Find all the dependencies here, so in each package user don't need to find the p
 - CKF
 #]]
 
+##############################################################################
+# import Gaudi at beginning
+##############################################################################
+find_package(Gaudi REQUIRED)
+
+
 find_package(CLHEP REQUIRED;CONFIG)
 find_package(DD4hep COMPONENTS DDCore DDG4 DDParsers DDRec REQUIRED)
+include(DD4hep) # include necessary macro
 find_package(EDM4HEP REQUIRED)
-#find_package(Garfield REQUIRED)
+find_package(Eigen3)
 find_package(Garfield)
 find_package(Geant4 REQUIRED ui_all vis_all)
+include(${Geant4_USE_FILE})
 find_package(GEAR REQUIRED)
+find_package(GenFit)
 find_package(GSL REQUIRED)
 find_package(HepMC)
 find_package(k4FWCore REQUIRED)
 find_package(LCContent REQUIRED)
 find_package(LCIO REQUIRED)
+find_package(OnnxRuntime REQUIRED)
 find_package(PandoraSDK REQUIRED)
 find_package(podio REQUIRED)
-find_package(ROOT COMPONENTS EG Graf Graf3d Gpad MathCore Net RIO Tree TreePlayer REQUIRED)
-find_package(GenFit)
+find_package(ROOT COMPONENTS EG Geom GenVector Graf Graf3d Gpad MathCore Net RIO Tree TreePlayer REQUIRED)
 
 if (CEPCSW_USE_SYSTEM_CKF_BELLE)
   message("Try to use an existing installation of CKF BELLE")
