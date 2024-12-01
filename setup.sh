cfe7978b9ff7d5c51357f1361754be3eb6775b03
lintao@ihep.ac.cn <lintao@ihep.ac.cn>
2024-12-01 07:05:46 +0000

    WIP: try to support LCG 105 for both centos7 and el9

--- setup.sh
+++ setup.sh
@@ -32,19 +32,32 @@ function cepcsw-external() {
 }
 
 function setup-external() {
-    local f=$(cepcsw-external)/setup-${CEPCSW_LCG_VERSION}.sh
-    if [ ! -f $f ]; then
-        error: "Failed to find setup script $f"
+    local f
+
+    # try new os
+    f=$(cepcsw-external)/setup-${CEPCSW_LCG_VERSION}-${CEPCSW_LCG_OS}.sh
+    if [ -f "$f" ]; then
+        info: "Setup CEPCSW externals (os: $CEPCSW_LCG_OS, lcg: $CEPCSW_LCG_VERSION): $f"
+        source $f
         return
     fi
 
-    info: "Setup CEPCSW externals: $f"
-    source $f
+    # legacy
+    if [ "$CEPCSW_LCG_OS" = "centos7" ]; then
+        f=$(cepcsw-external)/setup-${CEPCSW_LCG_VERSION}.sh
+        if [ -f "$f" ]; then
+            info: "Setup CEPCSW externals (os: $CEPCSW_LCG_OS, lcg: $CEPCSW_LCG_VERSION): $f"
+            source $f
+            return
+        fi
+    fi
+
+    error: "Failed to find setup script for CEPCSW_LCG_VERSION: ${CEPCSW_LCG_VERSION}, CEPCSW_LCG_OS: ${CEPCSW_LCG_OS}"
 
 }
 
-function setup-install-area() {
-    local installarea=$THISSCRITDIR/InstallArea
+function setup-install-area() { 
+    local installarea=$THISSCRITDIR/InstallArea/$CEPCSW_LCG_PLATFORM
     if [ ! -d "$installarea" ]; then
         info: "The InstallArea is not created yet. Please run ./build.sh first, then source setup.sh again."
         return
@@ -64,16 +77,58 @@ function setup-install-area() {
     info: "Setup CEPCSW: $installarea"
 }
 
+function detect-os() {
+    local os=$(source /etc/os-release; echo $ID)
+    local os_ver=$(source /etc/os-release; echo $VERSION_ID)
+
+    local cepcsw_lcg_os
+    
+    case "$os" in
+        almalinux)
+            cepcsw_lcg_os=el
+        ;;
+        centos)
+            cepcsw_lcg_os=centos
+        ;;
+    esac
+
+    case "$os_ver" in
+        7*)
+            cepcsw_lcg_os=${cepcsw_lcg_os}7
+            ;;
+        9*)
+            cepcsw_lcg_os=${cepcsw_lcg_os}9
+            ;;
+    esac
+
+    export CEPCSW_LCG_OS=${cepcsw_lcg_os}
+
+}
+
 ##############################################################################
 # Parse the command line options
 ##############################################################################
 
-# CEPCSW_LCG_VERSION=${1}; shift
 
+# CEPCSW_LCG_OS: centos7 or el9
+if [ -z "$CEPCSW_LCG_OS" ]; then
+    # try to detect os
+    detect-os
+fi
+
+# if user don't specify the version, try to detect according to the current os
 if [ -z "$CEPCSW_LCG_VERSION" ]; then
-    CEPCSW_LCG_VERSION=103.0.2
+    case "$CEPCSW_LCG_OS" in
+        el9)
+            CEPCSW_LCG_VERSION=105.0.0
+            ;;
+        *)
+            CEPCSW_LCG_VERSION=103.0.2
+            ;;
+    esac
 fi
 export CEPCSW_LCG_VERSION
+
 
 setup-external
 setup-install-area
